a33aa36ea117b46fadb5257b5b5067d3ace0a883
Jeevan Tamang <np03cs4a220026@heraldcollege.edu.np>
2023-01-22 00:30:46 +0545

    chnages

---
+++ webtechnology/Workshop 8 and 9/script1.js
@@ -0,0 +1,29 @@
+// // Values and variables
+// let country = 'Nepal';
+// let continent = 'Asian';
+// let population = 29136808;
+// console.log(typeof country)
+// console.log(typeof continent)
+// console.log(typeof population)
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+
+
+// // data Types
+
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland)
+// console.log(typeof language)
+
+//let const and var
+
+let language = 'Neplali';
+const country = 'Nepal';
+const continent = 'Asian';
+const isIsland = false;
+console.log(language)
+console.log(country)
+console.log(isIsland) 
+console.log(continent)
