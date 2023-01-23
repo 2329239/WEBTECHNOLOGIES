afaf92f2d1f4ccfb4d5ff7d5a4a375ea42545c10
Jeevan Tamang <np03cs4a220026@heraldcollege.edu.np>
2023-01-23 09:37:19 +0545

    changes

--- webtechnology/Workshop 8 and 9/script1.js
+++ webtechnology/Workshop 8 and 9/script1.js
@@ -17,13 +17,37 @@
 // console.log(typeof isIsland)
 // console.log(typeof language)
 
-//let const and var
+// let const and var
 
-let language = 'Neplali';
-const country = 'Nepal';
-const continent = 'Asian';
-const isIsland = false;
-console.log(language)
-console.log(country)
-console.log(isIsland) 
-console.log(continent)
+// let language = 'Neplali';
+// const country = 'Nepal';
+// const continent = 'Asian';
+// const isIsland = false;
+// isIsland = true; // it will show an error as const value value cannot be changeable
+// console.log(isIsland)
+
+//Basic operators
+
+let population = 29136808;
+let country = 'Nepal';
+let continenet = 'Asian';
+let language = 'Nepali';
+console.log(population/2);
+population++;
+console.log(population)
+console.log(population>6);
+console.log(population<33);
+// const description1 =
+// country +
+// ' is in ' +
+// continenet +
+// ', and its ' +
+// population +
+// ' million people speak ' +
+// language;
+// console.log(description1);
+
+// Strings and Template Literals
+ 
+const description = '${country} is in ${continent}, and its ${population} milliom speak ${language}';
+console.log(description)
